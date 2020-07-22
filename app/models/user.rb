edccae8c6e25d97bb56d0072973f712ac38e5c5b
bian160928 <biankobayashi@gmail.com>
2020-07-22 21:57:45 +0900

    create_favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+  validates :email, presence: true, uniqueness: true
+  
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts, only: [:index, :new, :create, :show, :destroy] do
-    collection do
-      get 'search'
-    end
+    resources :likes, only: [:create, :destroy]
+      collection do
+        get 'search'
+      end
   end
   resources :users, only: :show
 end
