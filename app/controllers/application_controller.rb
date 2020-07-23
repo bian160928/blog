d6b5602228bf20aece55e11930ac831ecf75cbcf
bian160928 <biankobayashi@gmail.com>
2020-07-23 17:46:22 +0900

    makeup_layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user!
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
