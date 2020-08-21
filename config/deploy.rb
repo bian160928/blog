a20499f0552ae2e6660daa1da9902c99db61e4de
bian160928 <biankobayashi@gmail.com>
2020-08-21 19:07:26 +0900

    jdfvojef

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,42 @@
 # config valid for current version and patch releases of Capistrano
-lock “3.14.1”
+lock "3.14.1"
 
-set :application, “blog”
-set :repo_url,  'git@github.com:bian160928/blog.git’
-set :linked_dirs, fetch(:linked_dirs, []).push(‘log’, ‘tmp/pids’, ‘tmp/cache’, ‘tmp/sockets’, ‘vendor/bundle’, ‘public/system’, ‘public/uploads’)
+set :application, "blog"
+set :repo_url,  'git@github.com:bian160928/blog.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :rbenv_type, :user
-set :rbenv_ruby, ‘2.6.5’
+set :rbenv_ruby, '2.6.5'
+
 # chat-spaceで使ったpemを指定
-set :ssh_options, auth_methods: [‘publickey’],
-                  keys: [‘~/.ssh/chatspace.pem’]
-set :unicorn_pid, -> { “#{shared_path}/tmp/pids/unicorn.pid” }
-set :unicorn_config_path, -> { “#{current_path}/config/unicorn.rb” }
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/chatspace.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
 set :linked_files, %w{ config/master.key }
-after ‘deploy:publishing’, ‘deploy:restart’
+
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke ‘unicorn:stop’
-    invoke ‘unicorn:start’
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
   end
-  desc ‘upload master.key’
+
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
-      if test “[ ! -d #{shared_path}/config ]”
-        execute “mkdir -p #{shared_path}/config”
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
       end
-      upload!(‘config/master.key’, “#{shared_path}/config/master.key”)
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
-  before :starting, ‘deploy:upload’
-  after :finishing, ‘deploy:cleanup’
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 
